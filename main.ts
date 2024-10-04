7347a839bb4bedbb131a003336e9ff92b9d750c3
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-04 16:46:42 -0400

    changed code

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Francisco
+ * Created on: Oct 2024
+ * This program acts as a replica of cookie clicker
 */
 
-basic.showString('Hello, World!')
+let cookieNumber = 0
+basic.showNumber(cookieNumber)
+input.onButtonPressed(Button.A, function () {
+    cookieNumber += 1
+    basic.showNumber(cookieNumber)
+})
+
+input.onButtonPressed(Button.B, function () {
+    cookieNumber = 0
+    basic.showNumber(cookieNumber)
+})
+
